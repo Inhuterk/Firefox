1dd2559dd8a40533a966b7de6bc30f19ed2c6ee0
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-01 18:51:08 +0530

    Update and rename Boot.sh to Booot.sh

--- Booot.sh
+++ Booot.sh
@@ -18,14 +18,11 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | tar -xzvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-3proxy-0.8.6 || exit 1
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    update-rc.d 3proxy defaults
-    cd $WORKDIR
+    cd "$WORKDIR" || exit 1
 }
 
 gen_3proxy() {
@@ -39,33 +36,31 @@ setuid 65535
 flush
 auth strong
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' "${WORKDATA}")
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+"flush\n"}' "${WORKDATA}")
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/48"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/48"}' "${WORKDATA}")
 EOF
 }
 
-# ... (rest of the functions remain unchanged)
-
 echo "installing apps"
 apt-get update
-apt-get install -y gcc net-tools libssl-dev bsdtar zip >/dev/null
+apt-get install -y gcc net-tools libssl-dev bsdtar zip || exit 1
 
 install_3proxy
 
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir "$WORKDIR" && cd "$_" || exit 1
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -73,26 +68,21 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
 echo "How many proxy do you want to create? Example 500"
-read COUNT
+read -r COUNT
 
 FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+LAST_PORT=$((FIRST_PORT + COUNT))
 
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+gen_data >"${WORKDIR}/data.txt"
+gen_iptables >"${WORKDIR}/boot_iptables.sh"
+gen_ifconfig >"${WORKDIR}/boot_ifconfig.sh"
+chmod +x "${WORKDIR}/boot_"*.sh
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-service 3proxy start
-EOF
+# Instead of modifying /etc/rc.local, consider using systemd services or other methods for starting services on boot
 
-bash /etc/rc.local
+service 3proxy start
 
 gen_proxy_file_for_user
 
