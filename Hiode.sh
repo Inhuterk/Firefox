be5f4a87321b888137503f50d44bc696e333bfc1
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-22 18:07:13 +0530

    Update Hiode.sh

--- Hiode.sh
+++ Hiode.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-
 random() {
 	tr </dev/urandom -dc A-Za-z0-9 | head -c5
 	echo
 }
 
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
@@ -33,18 +39,28 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
+auth strong
 
-$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\n"}' "${WORKDATA}")
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' "${WORKDATA}")
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' "${WORKDATA}")
 EOF
 }
 
 gen_proxy_file_for_user() {
-    awk -F "/" '{print $1 ":" $2}' ${WORKDATA} > proxy.txt
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
 }
 
 upload_proxy() {
-    zip proxy.zip proxy.txt
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
     echo "Proxy is ready! Format IP:PORT"
+    echo "Password: ${PASS}"
 }
 
 gen_data() {
@@ -54,9 +70,16 @@ gen_data() {
 }
 
 gen_iptables() {
-    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $2 " -j ACCEPT"}' ${WORKDATA} > boot_iptables.sh
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
 }
 
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
 echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
@@ -70,22 +93,24 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "External IP for IPv4 = ${IP4}. External subnet for IPv6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "How many proxies do you want to create? Example: 500"
+echo "How many proxy do you want to create? Example 500"
 read COUNT
 
 FIRST_PORT=22000
 LAST_PORT=22099
 
-gen_data > $WORKDIR/data.txt
-gen_iptables
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 systemctl start 3proxy
 EOF
