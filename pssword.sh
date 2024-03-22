d1a2d200c454217930cffb84e2112fec6d2d671f
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-22 16:49:28 +0530

    Update pssword.sh

--- pssword.sh
+++ pssword.sh
@@ -89,7 +89,7 @@ WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 IP4="172.105.158.155"
-IP6=$(curl -6 -s icanhazip.com | cut -f1-2 -d':')
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
