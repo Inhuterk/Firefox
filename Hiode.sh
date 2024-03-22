e6c8edddfb0f77d6f137adc6b8634bd8872f4f35
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-22 17:57:58 +0530

    Update Hiode.sh

--- Hiode.sh
+++ Hiode.sh
@@ -5,15 +5,6 @@ random() {
 	echo
 }
 
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
-gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
-
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
@@ -43,14 +34,12 @@ setuid 65535
 stacksize 6291456 
 flush
 
-$(awk -F "/" '{print "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n"}' "${WORKDATA}")
+$(awk -F "/" '{print "proxy -6 -n -a -p" $2 " -i" $1 " -e"$3"\n"}' "${WORKDATA}")
 EOF
 }
 
 gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4}' ${WORKDATA})
-EOF
+    awk -F "/" '{print $1 ":" $2}' ${WORKDATA} > proxy.txt
 }
 
 upload_proxy() {
@@ -60,20 +49,12 @@ upload_proxy() {
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$IP4/$port/$(gen64 $IP6)"
+        echo "$IP4/$port"
     done
 }
 
 gen_iptables() {
-    cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $2 " -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $3 "/64"}' ${WORKDATA})
-EOF
+    awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $2 " -j ACCEPT"}' ${WORKDATA} > boot_iptables.sh
 }
 
 echo "installing apps"
@@ -89,7 +70,7 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. External subnet for IPv6 = ${IP6}"
+echo "External IP for IPv4 = ${IP4}. External subnet for IPv6 = ${IP6}"
 
 echo "How many proxies do you want to create? Example: 500"
 read COUNT
@@ -97,16 +78,14 @@ read COUNT
 FIRST_PORT=22000
 LAST_PORT=22099
 
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+gen_data > $WORKDIR/data.txt
+gen_iptables
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 systemctl start 3proxy
 EOF
