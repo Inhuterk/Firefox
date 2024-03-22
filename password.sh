14e1b7dbce6ed427db2dec91c8ed10bb0e81fc23
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-22 16:33:44 +0530

    Update password.sh

--- password.sh
+++ password.sh
@@ -90,11 +90,11 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s icanhazip.com)
+IP4=$(curl -4 -s 172.105.158.155)
 
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = 172.105.158.155. External sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
 echo "How many proxy do you want to create? Example 500"
 read COUNT
